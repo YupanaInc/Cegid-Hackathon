1d455ab663545e9d594bce96e0e1eb804676da08
AlexYup <alex@loopsoftware.fr>
2019-07-02 17:06:56 +0100

    mail manager interface

---
+++ MailManager.js
@@ -0,0 +1,25 @@
+class MailManager {
+    constructor() {
+    }
+
+    /**
+     *
+     * @param {Date} fromDate
+     * @param {string[]} withTags
+     * @param {string[]} withoutTags
+     * @return {{id:string, subject:string}[]}
+     */
+    getEmails(fromDate, withTags, withoutTags) {
+
+    }
+
+    /**
+     *
+     * @param {string} emailId
+     * @param {string} tag
+     */
+    setTag(emailId, tag) {
+    }
+}
+
+exports.MailManager = new MailManager();
