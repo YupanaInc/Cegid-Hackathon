4277e5e5fc3992ef81e725edc894355ff88c90e4
Loic PORTALES <loic@portales.fr>
2019-07-02 15:50:34 +0200

    Mock data for bank statement

---
+++ mock-bank-statement.js
@@ -0,0 +1,32 @@
+
+const mockDataBankStatement = [
+    {
+        date: new Date(2019, 5, 13),
+        label: 'Amazon UK',
+        amount: 178.95
+    },
+    {
+        date: new Date(2019, 5, 15),
+        label: 'Pizza Express',
+        amount: 23.45
+    },
+    {
+        date: new Date(2019, 5, 18),
+        label: 'Apple - Macbook',
+        amount: 2599.50
+    },
+    {
+        date: new Date(2019, 5, 18),
+        label: 'Google Suite Subscription',
+        amount: 9.00
+    },
+    {
+        date: new Date(2019, 5, 19),
+        label: 'Ibis Budget - Rueil Malmaison',
+        amount: 123.78
+    }
+];
+
+
+console.log(mockDataBankStatement);
+
