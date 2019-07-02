70102fc083b5fe309e5f7b517e207324ea2696f7
jspelerin <jean-sebastien@loopsoftware.fr>
2019-07-02 23:01:28 +0200

    Changes for Vente invoice

--- category/mockPIA.js
+++ category/mockPIA.js
@@ -1,37 +1,41 @@
 const metadata = [
     {
-        "subject": "Amazon - Invoice 28/06/2019",
-        "documentId" : "74667120-b9f4-42a5-8e07-e1b6bad086cf",
-        "date" : "2019-07-02T15:50:10.411Z",
-        "label" : "Amazon",
-        "amount" : 179.0,
-        "type": "Achat"
+        subject: "Amazon",
+        documentId : "74667120-b9f4-42a5-8e07-e1b6bad086cf",
+        date : "2019-07-02T15:50:10.411Z",
+        label : "Amazon",
+        amount : 179.0,
+        type: "Achat"
     },
     {
-        "subject": "Facture relative à votre séjour - ibis budget Rueil Malmaison (F) / Your invoice at ibis budget Rueil Malmaison (F)",
-        "documentId" : "84667120-b9f4-42a5-8e07-e1b6bad086cf",
-        "date" : "2019-07-02T16:50:10.411Z",
-        "label" : "Ibis",
-        "amount" : 144.58,
-        "type": "Achat"
+        subject: "IBIS",
+        documentId : "84667120-b9f4-42a5-8e07-e1b6bad086cf",
+        date : "2019-07-02T16:50:10.411Z",
+        label : "Ibis",
+        amount : 144.58,
+        type: "Achat"
     },
     {
-        "documentId" : "84667120-b9f4-42a5-8e07-e1b6bad086cf",
-        "date" : "2019-07-02T16:50:10.411Z",
-        "label" : "Netflix",
-        "amount" : 8.58,
-        "type": "Achat"
+        subject: "Google",
+        documentId : "84667120-b9f4-42a5-8e07-e1b6bad086cf",
+        date : "2019-07-02T16:50:10.411Z",
+        label : "Google",
+        amount : 55.08,
+        type: "Vente"
     }
 ];
 
-
 function processDocuments(mails = []) {
-    return mails.map(({subject, id}) => {
+    return mails.map(({subject, id, userEmail}) => {
        return {
-           ...metadata.find((meta) => meta.subject === subject),
-           emailId: id
+           ...metadata.find((meta) => {
+               return subject.toLowerCase().indexOf(meta.subject.toLowerCase()) > -1;
+            }),
+            subject,
+            emailId: id,
+            userEmail
        }
-    });
+    }).filter(({documentId}) => documentId)
 }
 
 module.exports = {
