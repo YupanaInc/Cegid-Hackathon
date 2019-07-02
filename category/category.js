e7440f31eb92c55eb9997cba928d52a68810b26b
jspelerin <jean-sebastien@loopsoftware.fr>
2019-07-02 21:17:32 +0200

    Add tags

--- category/category.js
+++ category/category.js
@@ -2,15 +2,13 @@
 const {getMatchersCollection} = require("./mongodbConnect.js")
 const {intervalSeconds} = require('../config.js');
 const {processDocuments} = require("./mockPIA.js");
-const {fetchMails, updateTags} = require("./mockMails.js");
+const {fetchMails, updateTags} = require("./updateMails.js");
 
-function getTags(mailsOfInterest, documents) {
-    return mailsOfInterest.map(({emailId}, index) => {
+function getTags(documents) {
+    return documents.map(({emailId}, index) => {
         const {type} = documents[index];
         return {
-            tag: {
-                type
-            },
+            tag: type,
             emailId,
         };
     })
@@ -31,20 +29,19 @@ function createMatchers(_docs, _mailInformations) {
 async function main () {
     console.log("starting main");
     const mails = await fetchMails();
-    const mailsOfInterest = mails.filter(({attachment}) => {
-        return attachment && attachment.length;// get mails of interest
+    const mailsOfInterest = mails.filter(({categories}) => {
+        return categories && !categories.length;// get mails of interest
     });
 
-    const documents = await processDocuments(mailsOfInterest);
+    const mailsInfo = await processDocuments(mailsOfInterest);
     // Tag documents
-    const mailsInformation = getTags(mailsOfInterest, documents);
-    const done = await updateTags(mailsInformation);
-    // Flag emails
+    const tagsForMail = getTags(mailsInfo);
+    const done = await updateTags(tagsForMail);
+    // Flag emails\\
     if (done) {
         const collectionMatchers = await getMatchersCollection();
-        const matchers = createMatchers(documents, mailsOfInterest)
+        const matchers = createMatchers(mailsInfo, mailsOfInterest)
         await collectionMatchers.insert(matchers);
-        console.log("insert matchers");
     }
 
     setTimeout(main, intervalSeconds * 1000);
