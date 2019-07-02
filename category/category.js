ab2aa7cbcf1c657591fbc6ed39e3a554df33f4ba
jspelerin <jean-sebastien@loopsoftware.fr>
2019-07-02 20:09:36 +0200

    Add mock mail + add insert documeents

--- category/category.js
+++ category/category.js
@@ -1,51 +1,51 @@
 
 const {getMatchersCollection} = require("./mongodbConnect.js")
 const {intervalSeconds} = require('../config.js');
+const {processDocuments} = require("./mockPIA.js");
+const {fetchMails, updateTags} = require("./mockMails.js");
 
-// REST POST DOCUMENT FROM PIA => RETURN METADATA // mock
-
-
-
-
-
-// GET LIST OF LAST MAILS => API from Alex
-async function fetchMails() {
-    return {
-
-    }
+function getTags(mailsOfInterest, documents) {
+    return mailsOfInterest.map(({emailId}, index) => {
+        const {type} = documents[index];
+        return {
+            tag: {
+                type
+            },
+            emailId,
+        };
+    })
 }
 
-
-async function processPIA() {
-    // mock ?
-
-}
-
-
-async function tagDocuments() {
-    // mock
-
-
+function createMatchers(_docs, _mailInformations) {
+    return _docs.map((doc, index) => {
+        const {userMail, emailId} = _mailInformations[index];
+        return {
+            userMail,
+            emailId,
+            ...doc
+        }
+    });
 }
 
 
 async function main () {
     console.log("starting main");
+    const mails = await fetchMails();
+    const mailsOfInterest = mails.filter(({attachment}) => {
+        return attachment && attachment.length;// get mails of interest
+    });
 
-    const collectionMatchers = await getMatchersCollection();
-    const cursorMatchers = await collectionMatchers.find({});
-    const matchers = await cursorMatchers.toArray();
-    console.log(matchers);
-
-    // const mails = await fetchMails();
-
-    // const interestMails = mails.filter((mail) => {
-    //     // get mails of interest
-    //     return true;
-    // });
-
-    // const documents = await processPIA(interestMails);
-
+    const documents = await processDocuments(mailsOfInterest);
+    // Tag documents
+    const mailsInformation = getTags(mailsOfInterest, documents);
+    const done = await updateTags(mailsInformation);
+    // Flag emails
+    if (done) {
+        const collectionMatchers = await getMatchersCollection();
+        const matchers = createMatchers(documents, mailsOfInterest)
+        await collectionMatchers.insert(matchers);
+        console.log("insert matchers");
+    }
 
     setTimeout(main, intervalSeconds * 1000);
 };
